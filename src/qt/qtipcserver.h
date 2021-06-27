2d5d33cb65dcd4fe78ce2c13f3f0848452649901
Developer Betsplayer <developer.betsplayer@gmail.com>
2021-06-27 16:46:57 -0300

    a
    27-06

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Betsplayer-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "BetsplayerURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
